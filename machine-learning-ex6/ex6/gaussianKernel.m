8858d2a2a4f5ed612b571a428af24bf51a328cb5
Karanbir Chahal <karanbleep@gmail.com>
2016-01-18 20:24:19 +0530

    Week 8 Solutions. K means and Principal Component Analysis

--- machine-learning-ex6/ex6/gaussianKernel.m
+++ machine-learning-ex6/ex6/gaussianKernel.m
@@ -4,7 +4,19 @@ function sim = gaussianKernel(x1, x2, sigma)
 %   and returns the value in sim
 
 % Ensure that x1 and x2 are column vectors
+
+[r c] = size(x1);
+[l d] = size(x2);
+
+fprintf('Size:\nx1: %d %d\n x2: %d %d',r,c,l,d);
+
+
 x1 = x1(:); x2 = x2(:);
+
+[r c] = size(x1);
+[l d] = size(x2);
+
+fprintf('Size:\nx1: %d %d\n x2: %d %d',r,c,l,d);
 
 % You need to return the following variables correctly.
 sim = 0;
@@ -16,8 +28,13 @@ sim = 0;
 %
 %
 
+temp = 0;
+for i=1:size(x1),
+    temp = temp + (x1(i) - x2(i))*(x1(i) - x2(i));
+end;
 
 
+sim = exp(-temp/(2*sigma*sigma));
 
 
 
